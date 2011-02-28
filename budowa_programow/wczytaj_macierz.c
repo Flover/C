8470a0b0ed7cdbce2f6123f2c0cef37a1f8b8075
Mateusz <emflover@gmail.com>
2011-02-28 19:10:46 +0100

    add budowa_programow

---
+++ budowa_programow/wczytaj_macierz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void wczytaj_macierz(int n, float macierz[n][n])
+{
+    int x,y;
+    for (x = 0; x < n; x = x + 1) {
+        for (y = 0; y < n; y = y + 1) {
+            printf(" podaj element macierz[%i,%i]=", x, y);
+            scanf("%f", &macierz[x][y]);
+        }
+        printf("\n");
+    }
+}
+
+int wczytaj_wymiar_macierzy()
+{
+    extern int n;
+    printf("podaj wymair macierzy kwadratowej: ");
+    scanf("%i", &n);
+    printf("\n");
+    return n;
+}
---
+++ budowa_programow/wyznacznik.h
@@ -0,0 +1,3 @@
+/* funkcja oblicza wyznacznik maciery kwadratowej */
+
+float wyznacznik(int n, float macierz[n][n]);
