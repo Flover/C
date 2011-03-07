abc6f739fc8643ca1592f05b3b281d41def919e8
Mateusz <emflover@gmail.com>
2011-03-07 15:21:49 +0100

    homework

---
+++ budowa_programow/lab-01/zad2/helpers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int pobierz_calkowita(void){
+  return 0;
+}
+float x;
+double pobierz_rzeczywista(void){
+  printf("Podaj liczbe rzeczywista: ");
+  scanf("%f", &x);
+  return x;
+}
