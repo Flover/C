1d68913a2da9413a6b02b23899fcd7694cd6fbb4
Mateusz <emflover@gmail.com>
2011-05-10 16:27:48 +0200

    Szkielet kwadrat.c

---
+++ wskazniki/kwadrat.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+/* oblicz kwadrat liczby n */
+void kwadrat(int *n)
+{
+}
+void wczytaj_liczbe(int *n)
+{
+    printf("Wpisz liczbę naturalną: ");
+}
+
+int main()
+{
+    int liczba = 2, wynik = 4;
+    wczytaj_liczbe(&liczba);
+    kwadrat(&wynik);
+    printf("\nPodano liczbe: %d, jej kwadrat to: %d\n", liczba, wynik);	/* wypisz wczytaną liczbę oraz jej kwadrat */
+    return 0;
+}
